8fbc29728a36185503f75ac55725696ff248ba76
Kouki Saito <dan.addr.skd@gmail.com>
2016-07-05 03:48:01 +0900

    WIP: using interactive buttons

--- run.py
+++ run.py
@@ -1,4 +1,50 @@
-from slackbot.bot import Bot
+from slackbot.bot import Bot, respond_to
+import re
+
+attachments = [
+        {
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "chess",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "maze",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "war",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }
+    ]
+
+
+import json
+@respond_to('test', re.IGNORECASE)
+def test(message):
+    message.send_webapi('', json.dumps(attachments))
+
+
 def main():
     bot = Bot()
     bot.run()
